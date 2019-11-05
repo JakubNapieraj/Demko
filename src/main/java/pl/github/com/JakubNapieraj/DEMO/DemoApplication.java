ba322c434a735d3c36ad37d429825ce535eb5727
Kuba Napieraj <jakubnapieraj@gmail.com>
2019-11-05 19:24:08 +0100

    Initial commit

---
+++ src/main/java/pl/github/com/JakubNapieraj/DEMO/DemoApplication.java
@@ -0,0 +1,13 @@
+package pl.github.com.JakubNapieraj.DEMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
